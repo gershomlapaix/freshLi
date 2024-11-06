3e6055d2c09e96c41e59777cbc7f1e7b1c2b9c05
gershomlapaix <nsengiyumv33@gmail.com>
2024-11-07 00:30:18 +0200

    feat: authentication

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express, { json } from 'express';
 import { db } from './src/config/db.js';
+import authRouter from './src/routes/authRoutes.js';
 
 const app = express();
 
 app.use(json());
+
+app.use("/auth", authRouter)
 
 const port = 3000 || process.env.PORT;
 app.listen(port, () => {
---
+++ src/controllers/authController.js
@@ -0,0 +1,32 @@
+import bcrypt from 'bcryptjs'
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const register = async (req, res) => {
+    const { username, email, password, role } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await User.create({ username, email, password: hashedPassword, role });
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (err) {
+        res.status(500).json({ message: 'Error registering user', error: err });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findByEmail(email);
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });        
+
+        const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (err) {
+        console.log(err);
+        
+        res.status(500).json({ message: 'Error logging in', error: err });
+    }
+};
